a99bd13da507e5695ed8af1550a1cd951a377c19
github username <github email address>
2020-05-11 14:15:16 -0500

    Added error displays per each feild

--- CodeKing/config/routes.rb
+++ CodeKing/config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
-  resources :repos do
-    # nested resource for pushes
-    resources :pushes, only: [:show, :index, :new, :create]
-  end
-  resources :pushes, only: [:show, :index, :new, :create]
+
+  #home
   root "home#index"
-  
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+
+  #users
   get "signup", to: "users#new", as: "signup"
+  resources :users, only: [:show, :index, :new, :create, :edit, :update, :delete, :destroy]
+
+  #sessions
   get "login", to: "sessions#new", as: "login"
   get "logout", to: "sessions#destroy", as: "logout"
+  resources :sessions, only: [:new, :create, :destroy]
+
+  #repos
+  resources :repos, only: [:show, :index, :new, :create, :edit, :update, :delete, :destroy] do
+    # nested resource for pushes
+    resources :pushes, only: [:show, :index, :new, :create, :edit, :update]
+  end
+
+  #pushes
+  resources :pushes, only: [:show, :index, :new, :create]
+
 end
