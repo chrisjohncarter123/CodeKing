a6796aed36e93da77a649c92aadddf95dfeb6190
github username <github email address>
2020-05-19 15:59:12 -0500

    Added named scope method for repo model

--- CodeKing/app/models/repo.rb
+++ CodeKing/app/models/repo.rb
@@ -4,10 +4,14 @@ class Repo < ApplicationRecord
     validates :name, presence: true, uniqueness: true
 
     scope :name_is_a, -> {where(name: "a")}
-
     def self.name_is_a
         where(name: "a")
 
+    end
+
+    scope :named, -> (name) {where("name LIKE ?", name)
+    def named(name)
+        where("name LIKE ?", name)
     end
 
 end
